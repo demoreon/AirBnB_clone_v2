945cc17bfb0ab10e740a9b23df4873626fb457ce
demoreon <demoreon@gmail.com>
2023-04-22 18:36:15 +0100

    web_flask upload

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -15,12 +15,9 @@ def teardown_session(exception):
 
     @app.route('/states_list', strict_slashes=False)
     def display_html():
-        """ Function called with /states_list route """
-        states = storage.all(State)
-        dict_to_html = {value.id: value.name for value in states.values()}
-        return render_template('7-states_list.html',
-                               Table="States",
-                               items=dict_to_html)
+        """ A swift function call"""
+        data = storage.all(State)
+            return render_template('7-states_list.html', total=data.values())
 
     if _fg_name__ == "__main__":
         app.run(host='0.0.0.0', port=5000)
