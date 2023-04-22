026fd1e77252efb839a6836597ad2f159d70bf5e
demoreon <demoreon@gmail.com>
2023-04-22 18:26:56 +0100

    web_flask upload

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-"""
-This script processes a Flask web APP.
-The APP listens on 0.0.0.0, port 5000.
-Routes:
-/states_list: HTML page with a list of all State objects in DBStorage.
-"""
+""" Script that runs an app with Flask framework """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -14,23 +9,18 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close_db(exc):
-    """close the current session of sqlalchemist"""
+def teardown_session(exception):
+    """ Teardown """
     storage.close()
 
+    @app.route('/states_list', strict_slashes=False)
+    def display_html():
+        """ Function called with /states_list route """
+        states = storage.all(State)
+        dict_to_html = {value.id: value.name for value in states.values()}
+        return render_template('7-states_list.html',
+                               Table="States",
+                               items=dict_to_html)
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """
-    Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
-    """
-    states = storage.all(State).values
-    dict_to_html = {value.id: value.name for value in states.values()}
-    return render_template('7-states_list.html',
-                           Table="States",
-                           items=dict_to_html)
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    if _fg_name__ == "__main__":
+        app.run(host='0.0.0.0', port=5000)
