f87ebda2a9a9e3dcfba44aac9660561a4fd54e39
demoreon <demoreon@gmail.com>
2023-04-24 11:55:48 +0100

    web_flask upload

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-A simple Flask web application that lists
-"""
-
+"""A Simple flask web application that lists states"""
 
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 
 app = Flask(__name__)
@@ -14,17 +10,18 @@ app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def close_db(exc):
-    """ close the current session of sqlalchemist """
+def close_storage(exception=None):
+    """Closes the current SQLAlchemy session"""
     storage.close()
 
 
 @app.route('/states_list')
-def states_list():
-    """ States are sorted by name. """
+def state_list():
+    """Returns a rendered HTML template"""
+    from models.state import State
     states = storage.all(State).values()
-    return render_template("7-states_list.html", states=states)
+    return render_template('7-states_list.html', states=states)
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+app.run(host="0.0.0.0", port=5000)
