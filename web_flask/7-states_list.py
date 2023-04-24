6e9b06378d90890e9b555dab91add99223378ced
demoreon <demoreon@gmail.com>
2023-04-24 19:47:27 +0100

    web_flask upload

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
+
+"""
+A Simple flask web application that lists states
+"""
+
 from flask import Flask, render_template
 from models import storage
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
-"""A Simple flask web application that lists states"""
 
 
 @app.teardown_appcontext
@@ -12,13 +17,15 @@ def close_storage(exception=None):
     storage.close()
 
 
-@app.route('/states_list')
-def state_list():
-    """Returns a rendered HTML template"""
-    from models.state import State
-    states = storage.all(State).values()
-    return render_template('7-states_list.html', states=states)
+    @app.route('/states_list')
+    def state_list():
+        """Returns a rendered HTML template"""
+        from models.state import State
+        states = storage.all(State).values()
+        return render_template('7-states_list.html',
+                               states=states)
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
+    
