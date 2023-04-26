7008c79de2ac575789c19a9b5a1e281200ef466d
demoreon <demoreon@gmail.com>
2023-04-26 08:03:09 +0100

    7- retest

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,46 +1,32 @@
 #!/usr/bin/python3
-"""A simple Flask web application that displays a list of states.
-
-This application listens on 0.0.0.0, port 5000. It retrieves a list of all State
-objects from a database using the DBStorage model in the 'models' module, and
-renders an HTML page with the list.
-
-To run the application, ensure that the following dependencies are installed:
-- Flask
-- models (imported from the 'models' module)
-
-Assumptions:
-- The application assumes that the database is set up correctly and contains a
-  table named 'states' with columns 'id' and 'name'.
+"""Starts a Flask web application.
 
 Routes:
-- /states_list: HTML page with a list of all State objects in DBStorage.
-
-Functions:
-- states_list: Retrieves a list of all State objects from the database and
-  renders an HTML page with the list.
-- teardown_appcontext: Closes the database connection after each request.
+    /states_list: Displays a list of all State objects in the database.
 
 """
+
 from models import storage
+from models.state import State
 from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage."""
-    states = storage.all("State")
-    return render_template("7-states_list.html", states=states)
+    """Displays a list of all State objects in the database."""
+    states = list(storage.all(State).values())
+    states.sort(key=lambda state: state.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_appcontext(exc):
-    """Closes the database connection after each request."""
+def teardown_appcontext(exception):
+    """Removes the current SQLAlchemy Session after each request."""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
