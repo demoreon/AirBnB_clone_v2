53acf9c83b7731bbd148c9711850ed8bf0e72eba
demoreon <demoreon@gmail.com>
2023-04-26 07:51:11 +0100

    7- retest

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,17 +14,15 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
+    """Displays an HTML page with a list of all State objects in DBStorage."""
 
-    States are sorted by name.
-    """
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Tear down session:db"""
+    """tear down session: db"""
     storage.close()
 
 
