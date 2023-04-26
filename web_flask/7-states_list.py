2ae1cf37109a1185342af74cb06291fffc54082c
demoreon <demoreon@gmail.com>
2023-04-26 08:46:10 +0100

    7- retest

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""Launches a simple Flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
